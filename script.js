b23318e1496656dcaae8117fe247cacf6a43b6af
Patryk Sawicki <sawicki.patryk129@gmail.com>
2023-11-25 22:13:56 +0100

    carousel in home finished

--- script.js
+++ script.js
@@ -10,3 +10,36 @@ toggle.addEventListener('click', () => {
     navBar.classList.remove('active')
 })
 
+const carousel = document.querySelector('.carousel')
+const arrowBtns = document.querySelectorAll('.arrow')
+const firstCardWith = carousel.querySelector('.card').offsetWidth
+
+let isDragging = false
+let startX = 0
+let startScrollLeft = 0
+
+arrowBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        carousel.scrollLeft += btn.id === 'left' ? -firstCardWith : firstCardWith
+    })
+})
+
+const startDrag = (e) => {
+    isDragging = true
+    carousel.classList.add('dragging')
+    startX = e.pageX
+    startScrollLeft = carousel.scrollLeft
+}
+const dragging = (e) => {
+    if (!isDragging) return
+    carousel.scrollLeft = startScrollLeft - (e.pageX - startX)
+}
+const stopDrag = () => {
+    isDragging = false
+    carousel.classList.remove('dragging')
+
+}
+
+carousel.addEventListener('mousemove', dragging)
+carousel.addEventListener('mousedown', startDrag)
+document.addEventListener('mouseup', stopDrag)
