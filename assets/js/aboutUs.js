5c3d23d79c67d1d3c49f80d815061be9a8fdf9f3
Patryk Sawicki <sawicki.patryk129@gmail.com>
2023-11-25 23:04:32 +0100

    carousel in aboutUs done

--- assets/js/aboutUs.js
+++ assets/js/aboutUs.js
@@ -9,3 +9,62 @@ toggle.addEventListener('click', () => {
     }
     navBar.classList.remove('active')
 })
+
+const carousel = document.querySelector('.carousel')
+const arrowBtns = document.querySelectorAll('.arrow')
+const firstCardWith = carousel.querySelector('.card').offsetWidth
+const carouselChildrens = [...carousel.children]
+
+let isDragging = false
+let startX = 0
+let startScrollLeft = 0
+
+
+let cardPerView = Math.round(carousel.offsetWidth / firstCardWith)
+
+carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
+    carousel.insertAdjacentHTML('afterbegin', card.outerHTML)
+})
+carouselChildrens.slice(0, -cardPerView).forEach(card => {
+    carousel.insertAdjacentHTML('beforeend', card.outerHTML)
+})
+
+arrowBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        carousel.scrollLeft += btn.id === 'left' ? -firstCardWith : firstCardWith
+    })
+})
+
+const startDrag = (e) => {
+    isDragging = true
+    carousel.classList.add('dragging')
+    startX = e.pageX
+    startScrollLeft = carousel.scrollLeft
+}
+const dragging = (e) => {
+    if (!isDragging) return
+    carousel.scrollLeft = startScrollLeft - (e.pageX - startX)
+}
+const stopDrag = () => {
+    isDragging = false
+    carousel.classList.remove('dragging')
+
+}
+const infiniteScroll = () => {
+    if (carousel.scrollLeft === 0) {
+
+        carousel.classList.add('no-transition')
+        carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth)
+        carousel.classList.remove('no-transition')
+
+    } else if (Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth) {
+        carousel.classList.add('no-transition')
+        carousel.scrollLeft = carousel.offsetWidth
+        carousel.classList.remove('no-transition')
+    }
+}
+
+carousel.addEventListener('mousedown', startDrag)
+carousel.addEventListener('mousemove', dragging)
+document.addEventListener('mouseup', stopDrag)
+carousel.addEventListener('scroll', infiniteScroll)
--- script.js
+++ script.js
@@ -13,10 +13,21 @@ toggle.addEventListener('click', () => {
 const carousel = document.querySelector('.carousel')
 const arrowBtns = document.querySelectorAll('.arrow')
 const firstCardWith = carousel.querySelector('.card').offsetWidth
+const carouselChildrens = [...carousel.children]
 
 let isDragging = false
 let startX = 0
 let startScrollLeft = 0
+
+
+let cardPerView = Math.round(carousel.offsetWidth / firstCardWith)
+
+carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
+    carousel.insertAdjacentHTML('afterbegin', card.outerHTML)
+})
+carouselChildrens.slice(0, -cardPerView).forEach(card => {
+    carousel.insertAdjacentHTML('beforeend', card.outerHTML)
+})
 
 arrowBtns.forEach(btn => {
     btn.addEventListener('click', () => {
@@ -39,7 +50,21 @@ const stopDrag = () => {
     carousel.classList.remove('dragging')
 
 }
+const infiniteScroll = () => {
+    if (carousel.scrollLeft === 0) {
 
+        carousel.classList.add('no-transition')
+        carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth)
+        carousel.classList.remove('no-transition')
+
+    } else if (Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth) {
+        carousel.classList.add('no-transition')
+        carousel.scrollLeft = carousel.offsetWidth
+        carousel.classList.remove('no-transition')
+    }
+}
+
+carousel.addEventListener('mousedown', dragStart)
 carousel.addEventListener('mousemove', dragging)
-carousel.addEventListener('mousedown', startDrag)
-document.addEventListener('mouseup', stopDrag)
+document.addEventListener('mouseup', dragStop)
+carousel.addEventListener('scroll', infiniteScroll)
